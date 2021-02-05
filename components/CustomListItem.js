b88297bab2d73b17c9ee3fd6e03de63a84b966f6
khr-rakib <khr.rakib@gmail.com>
2021-02-05 21:45:13 +0600

    add chating system

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -4,13 +4,13 @@ import {ListItem, Avatar} from 'react-native-elements'
 
 const CustomListItem = ({id, chatName, enterChat}) => {
     return (
-        <ListItem>
+        <ListItem key={id} bottomDivider onPress={() => enterChat(id, chatName)}>
             <Avatar
                 rounded
                 source={{uri: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"}}
             />
             <ListItem.Content>
-                <ListItem.Title style={{fontWeight: "800"}}>youtube chat</ListItem.Title>
+                <ListItem.Title style={{fontWeight: "800"}}>{chatName}</ListItem.Title>
                 <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">This is a test subtitle</ListItem.Subtitle>
             </ListItem.Content>
         </ListItem>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,14 +5,32 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import { Avatar } from 'react-native-elements'
 import CustomListItem from '../components/CustomListItem'
 import {auth, db} from '../firebase'
+import {AntDesign, SimpleLineIcons} from '@expo/vector-icons'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const HomeScreen = ({navigation}) => {
+    const [chats, setChats] = useState([]);
 
     const signOutUser = () => {
         auth.signOut().then(() => {
             navigation.replace('Login')
         })
     }
+
+    const enterChat = (id, chatName) => {
+        navigation.navigate('Chat', {id, chatName})
+    }
+
+    useEffect(() => {
+        const unsubscribe = db.collection('chats').onSnapshot(snapshot => (
+            setChats(snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data()
+            })))
+        ))
+        return unsubscribe;
+    }, [])
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -25,14 +43,26 @@ const HomeScreen = ({navigation}) => {
                         <Avatar rounded source={{uri: auth?.currentUser?.photoURL}} />
                     </TouchableOpacity>
                 </View>
+            ),
+            headerRight: () => (
+                <View style={{ flexDirection: 'row', justifyContent: "space-between", width: 80, marginRight: 20 }}>
+                    <TouchableOpacity activeOpacity={0.5}>
+                        <AntDesign name="camerao" size={24} color="black" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('AddChat')} activeOpacity={0.5}>
+                        <SimpleLineIcons name="pencil" size={24} color="black" />
+                    </TouchableOpacity>
+                </View>
             )
         })
-    }, [])
+    }, [navigation])
 
     return (
         <SafeAreaView>
-            <ScrollView>
-                <CustomListItem/>
+            <ScrollView style={styles.container}>
+                {chats.map(({id, data: {chatName}}) => (
+                    <CustomListItem key={id} id={id} chatName={chatName} enterChat={enterChat} />
+                ) )}
             </ScrollView>
         </SafeAreaView>
     )
@@ -40,4 +70,8 @@ const HomeScreen = ({navigation}) => {
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        height: "100%"
+    }
+})
